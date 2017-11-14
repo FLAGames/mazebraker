46638df2880f1f1a2885342ee29ebe59c226f8e4
dambergn <dambergn@gmail.com>
2017-11-14 12:14:29 -0800

    music function

--- map-test.js
+++ map-test.js
@@ -2,14 +2,14 @@
 
 var generateBlocks = function() {
   //     number            X,   Y,   W,  H
-    block [0] = new Blocks(100, 100, 20, 20);
-    block [1] = new Blocks(100, 600, 20, 20);
-    block [2] = new Blocks(350, 350, 20, 20);
-    block [3] = new Blocks(600, 100, 20, 20);
-    block [4] = new Blocks(600, 600, 20, 20);
-    block [5] = new Blocks(125, 100, 20, 20);
-    block [6] = new Blocks(600, 400, 20, 20);
-    block [7] = new Blocks(300, 100, 20, 20);
+  block [0] = new Blocks(100, 100, 20, 20);
+  block [1] = new Blocks(100, 600, 20, 20);
+  block [2] = new Blocks(350, 350, 20, 20);
+  block [3] = new Blocks(600, 100, 20, 20);
+  block [4] = new Blocks(600, 600, 20, 20);
+  block [5] = new Blocks(125, 100, 20, 20);
+  block [6] = new Blocks(600, 400, 20, 20);
+  block [7] = new Blocks(300, 100, 20, 20);
 };
 
 generateBlocks();
