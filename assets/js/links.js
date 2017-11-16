70fa44a88cad3ba232dba4c8ef7e2f1ea401edfd
dambergn <dambergn@gmail.com>
2017-11-15 18:21:18 -0800

    links.js

---
+++ assets/js/links.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function generateLinks(music){
+  document.write('<li><a href="index.html">Home</a></li>');
+  document.write('<li><a href="#">Link 1</a></li>');
+  document.write('<li><a href="#">Link 2</a></li>');
+  document.write('<li><a href="games.html">Games</a></li>');
+  document.write('<li><a href="#">Link Test</a></li>');
+};
+
+generateLinks();
